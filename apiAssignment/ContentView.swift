40309f79483577af8b9206c261d6ed53ff3114ca
Ananya Tandel <anta1515@colorado.edu>
2023-09-26 09:19:27 -0600

    content view update

--- apiAssignment/ContentView.swift
+++ apiAssignment/ContentView.swift
@@ -1,65 +1,80 @@
 //
-//  ContentView.swift
-//  apiAssignment
+// ContentView.swift
+// apiAssignment
 //
-//  Created by Ananya Tandel on 9/18/23.
-//
+// Created by Ananya Tandel on 9/18/23.
 
-import SwiftUI
-import CoreData
 
 import SwiftUI
 
-// api source/url: https://jokeapi.dev/
-
-// show the Joke data from the API
 struct Joke: Codable, Identifiable {
     var id: Int
     var category: String
     var type: String
     var joke: String
+    var flags: Flags
+    var safe: Bool
+    var lang: String
+    
+//    enum CodingKeys: String, CodingKey {
+//            case id = "id"
+//            case category
+//            case type
+//            case joke
+//            case flags
+//            case safe
+//            case lang
+//        }
 }
 
-// create the main view for displaying a list of jokes
+struct Flags: Codable {
+    var nsfw: Bool
+    var religious: Bool
+    var political: Bool
+    var racist: Bool
+    var sexist: Bool
+    var explicit: Bool
+}
+
 struct JokesView: View {
-    @State var jokes = [Joke]() // state property to store the fetched jokes
-    
-    // fetch jokes from the JokeAPI
-    func getAllJokes() async -> () {
+    @State var joke: Joke?  // Store a single joke
+
+    // fetch a joke from API
+    func getJoke() async {
         do {
-            let url = URL(string: "https://v2.jokeapi.dev/joke/Any")!
-            let (data, _) = try await URLSession.shared.data(from: url) // fetch data
-            
-            // json response into an array of Jokes
-            jokes = try JSONDecoder().decode([Joke].self, from: data)
+            // define url
+            let url = URL(string: "https://v2.jokeapi.dev/joke/Any?amount=20")!
+            // fetch data from url
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            // decode the response into a single Joke object
+            joke = try JSONDecoder().decode(Joke.self, from: data)
         } catch {
-            print("Error: \(error.localizedDescription)")
+            // error message
+            print(error)
         }
     }
-    
+
     var body: some View {
         NavigationView {
-            List(jokes) { joke in
-                // navigate to the JokeDetailView
-                NavigationLink(destination: JokeDetailView(joke: joke)) {
-                    VStack(alignment: .leading) {
-                        Text(joke.category)
-                        Text(joke.joke)
+            if let joke = joke {
+                JokeDetailView(joke: joke)
+            } else {
+                Text("Loading...")  // show a loading message
+                    .onAppear {
+                        Task {
+                            await getJoke()
+                        }
                     }
-                }
-            }
-            .task {
-                await getAllJokes() // display jokes when the view appears
             }
         }
-        .navigationTitle("Jokes") // title
+        .navigationTitle("Jokes") // nav title
     }
 }
 
-// detail view for displaying an individual joke
 struct JokeDetailView: View {
-    let joke: Joke // display joke
-    
+    let joke: Joke // display in detail view
+
     var body: some View {
         VStack {
             Text(joke.category)
@@ -67,12 +82,13 @@ struct JokeDetailView: View {
             Text(joke.joke)
                 .padding()
         }
-        .navigationTitle("Joke Detail") // detail view title
+        .navigationTitle("Joke Detail View") // detail view title
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        JokesView() // preview screen
+        JokesView()
     }
 }
+
